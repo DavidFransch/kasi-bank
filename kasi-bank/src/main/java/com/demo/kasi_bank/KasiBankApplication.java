f5e1bcb7b2ea461607ca622425c8194be8281002
david.fransch <djsfdavid@gmail.com>
2024-06-19 13:45:17 +0200

    chore: first commit

---
+++ kasi-bank/src/main/java/com/demo/kasi_bank/KasiBankApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kasi_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasiBankApplication.class, args);
+	}
+
+}
---
+++ kasi-bank/src/test/java/com/demo/kasi_bank/KasiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kasi_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
